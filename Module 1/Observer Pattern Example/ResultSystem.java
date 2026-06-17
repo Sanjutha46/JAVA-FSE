a9781f73cbcdab0ad774d9329b0ba5976fd36c43
Sanjutha46 <166809033+Sanjutha46@users.noreply.github.com>
2026-06-17 22:17:44 +0530

    Update ResultSystem.java

--- Module 1/Observer Pattern Example/ResultSystem.java
+++ Module 1/Observer Pattern Example/ResultSystem.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class ResultSystem {
+
+    private List<Observer> students = new ArrayList<>();
+
+    public void addStudent(Observer student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Observer student) {
+        students.remove(student);
+    }
+
+    public void publishResult() {
+        notifyStudents("Exam Results Published!");
+    }
+
+    private void notifyStudents(String message) {
+        for (Observer student : students) {
+            student.update(message);
+        }
+    }
+}
