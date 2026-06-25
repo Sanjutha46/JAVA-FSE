fb4217b07aafef9cf542e015b4cbc772cb225c32
Sanjutha46 <166809033+Sanjutha46@users.noreply.github.com>
2026-06-25 14:12:05 +0530

    Create CountryRepository.java

---
+++ Module 6/Hands on 7: Add a new country/src/main/java/com/cognizant/spring/learn/repository/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.cognizant.spring.learn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cognizant.spring.learn.model.Country;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
+
+}
