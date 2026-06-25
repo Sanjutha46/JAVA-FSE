0b1c320813e911c7b93051a891ccaad82c815562
Sanjutha46 <166809033+Sanjutha46@users.noreply.github.com>
2026-06-25 14:15:49 +0530

    Create data.sql

---
+++ Module 6/Hands on 7: Add a new country/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO country (co_code, co_name) VALUES ('IN', 'India');
+INSERT INTO country (co_code, co_name) VALUES ('US', 'United States');
+INSERT INTO country (co_code, co_name) VALUES ('GB', 'United Kingdom');
+INSERT INTO country (co_code, co_name) VALUES ('AX', 'Åland Islands');
