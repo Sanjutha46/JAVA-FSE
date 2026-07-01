5fdec933a1a4f8fd11724159eea2fe8e751875d7
Sanjutha46 <166809033+Sanjutha46@users.noreply.github.com>
2026-07-01 13:14:58 +0530

    Create SecurityConfig.java

---
+++ Module 7/Create authentication service that returns JWT/src/main/java/com/cognizant/jwt/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.cognizant.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        return new InMemoryUserDetailsManager(
+
+                User.withUsername("user")
+                        .password("{noop}pwd")
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .httpBasic();
+
+        return http.build();
+    }
+}
